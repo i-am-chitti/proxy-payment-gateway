a5d3dd2296e0dc3eacfa766d4a93a900d7fa65e7
Deepak Kumar <deepak.kumar852182@gmail.com>
2024-12-02 23:22:31 +0530

    Remove redundant code

--- backend/src/payment/payment.module.ts
+++ backend/src/payment/payment.module.ts
@@ -4,29 +4,13 @@ import { PaymentController } from './payment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
 import { Order } from 'src/payment/order.entity';
-import { ConfigService } from '@nestjs/config';
-import { EnvironmentService } from 'src/environment/environment.service';
 import { RazorpayService } from './razorpay.service';
 import { GatewayCredentialsModule } from 'src/gateway-credentials/gateway-credentials.module';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const Razorpay = require('razorpay');
+import { PhonepeService } from './phonepe.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Order]), GatewayCredentialsModule],
-  providers: [
-    PaymentService,
-    RazorpayService,
-    {
-      provide: 'RAZORPAY',
-      inject: [ConfigService],
-      useFactory: (environmentService: EnvironmentService) => {
-        return new Razorpay({
-          key_id: environmentService.get('RAZORPAY_KEY_ID'),
-          key_secret: environmentService.get('RAZORPAY_KEY_SECRET'),
-        });
-      },
-    },
-  ],
+  providers: [PaymentService, RazorpayService, PhonepeService],
   controllers: [PaymentController],
 })
 export class PaymentModule {}
